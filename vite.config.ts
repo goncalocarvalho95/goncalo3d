7d72b720ebf374aa5c29b9008ed05637b9d86448
GONCALO-LAPTOP\goncalo <goncalobenfiquista@hotmail.com>
2024-09-09 19:22:59 +0100

    Add vite project

---
+++ vite.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './',
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+    extensions: [
+      '.js',
+      '.json',
+      '.jsx',
+      '.mjs',
+      '.ts',
+      '.tsx',
+      '.vue',
+    ],
+  },
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: "../dist/",
+    emptyOutDir: true
+  }
+})
